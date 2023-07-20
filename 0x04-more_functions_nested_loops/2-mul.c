aa57c0f85aec2a3181b2a878c6b3b7c8dba3910f
Tess-Olere <omokpotessy@gmail.com>
2023-07-20 11:20:13 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * mull - func checks for checks for a digit (0 through).
+ * mul - function that multiplies 2 integers.
  * @a: a - Variable
  * @b: b - Variable
- * Return: Always 0.
+ * Return: a * b
  */
 int mul(int a, int b)
 {
