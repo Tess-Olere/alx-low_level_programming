dd51bb45de0433f375d297d47a0d1474331f216f
Tess-Olere <omokpotessy@gmail.com>
2023-07-26 11:04:44 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,7 +7,7 @@
  * Return: void
  */
 
-char *strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
@@ -18,7 +18,7 @@ char *strcat(char *dest, char *src)
 		i++;
 	}
 	j = 0;
-	while(src[j] != '\0')
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
