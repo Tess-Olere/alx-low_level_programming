79078fcb70555578d89ef2642f06afdb668b23d1
Tess-Olere <omokpotessy@gmail.com>
2023-08-08 08:54:26 -0700

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints all argument content
+ * @argc: The argument count
+ * @argv: The argument vector
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
